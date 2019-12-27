d6c384ff074d08c9ff3d755cf26a3a392520af77
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2019-12-27 15:34:50 +0200

    Login, Register + MongoDB

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express');
-//const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 const app = express();
 
 //Connect Database
-//connectDB();
+connectDB();
 
 //Init Middlawere
-//app.use(express.json({ extended: false }));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API RUNNING'));
 
 //Define routes
-// app.use('/api/users', require('./routes/api/users'));
-// app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 // app.use('/api/profile', require('./routes/api/profile'));
 // app.use('/api/posts', require('./routes/api/posts'));
 
