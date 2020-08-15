3933c5ca12fdbade0a48420fa95dcf77d712c0d5
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-08-15 20:32:34 +0300

    postbuild

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
 //Connect Database
 connectDB();
@@ -8,14 +9,21 @@ connectDB();
 //Init Middlawere
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API RUNNING'));
+app.get("/", (req, res) => res.send("API RUNNING"));
 
 //Define routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/movies', require('./routes/api/movies'));
-// app.use('/api/profile', require('./routes/api/profile'));
-// app.use('/api/posts', require('./routes/api/posts'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/movies", require("./routes/api/movies"));
+
+//Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
