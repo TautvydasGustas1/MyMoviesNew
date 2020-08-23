889cc98f3bff48518199be6bfd99928d8f370d08
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-08-23 22:41:53 +0300

    Fixed Search button on mobile

--- client/src/actions/movies.js
+++ client/src/actions/movies.js
@@ -1,107 +1,109 @@
-import axios from 'axios';
+import axios from "axios";
 import {
-    REQUEST_MOVIES_TRENDING,
-    SUCCESS_MOVIES_TRENDING,
-    ERROR_MOVIES_TRENDING,
-    REQUEST_SEARCH_MOVIES,
-    SUCCESS_SEARCH_MOVIES,
-    ERROR_SEARCH_MOVIES,
-    GET_MOVIE_DATA,
-    CLEAR_STATE,
-    SUCCESS_SEARCH_USERS,
-    ERROR_SEARCH_USERS,
-    REQUEST_SEARCH_USERS
-} from './types';
+  REQUEST_MOVIES_TRENDING,
+  SUCCESS_MOVIES_TRENDING,
+  ERROR_MOVIES_TRENDING,
+  REQUEST_SEARCH_MOVIES,
+  SUCCESS_SEARCH_MOVIES,
+  ERROR_SEARCH_MOVIES,
+  GET_MOVIE_DATA,
+  CLEAR_STATE,
+  SUCCESS_SEARCH_USERS,
+  ERROR_SEARCH_USERS,
+  REQUEST_SEARCH_USERS,
+} from "./types";
 
-const requestAction = typeName => {
-    return {
-        type: REQUEST_SEARCH_MOVIES
-    };
+const requestAction = (typeName) => {
+  return {
+    type: REQUEST_SEARCH_MOVIES,
+  };
 };
 
 const successAction = (data, typeName) => {
-    return {
-        type: typeName,
-        payload: data
-    };
+  return {
+    type: typeName,
+    payload: data,
+  };
 };
 
 const errorAction = (error, typeName) => {
-    return {
-        type: typeName,
-        payload: error
-    };
+  return {
+    type: typeName,
+    payload: error,
+  };
 };
 
-export const getTrendingMovies = page => async dispatch => {
-    dispatch(requestAction(REQUEST_MOVIES_TRENDING));
-    await axios
-        .get(`/api/movies/trending/movies?page=${page}`)
-        .then(response => {
-            dispatch(successAction(response.data, SUCCESS_MOVIES_TRENDING));
-        })
-        .catch(error => {
-            dispatch(errorAction(error.message, ERROR_MOVIES_TRENDING));
-        });
-};
-
-export const getPopularMovies = page => async dispatch => {
-    dispatch(requestAction(REQUEST_MOVIES_TRENDING));
-    await axios
-        .get(`/api/movies/popular/movies?page=${page}`)
-        .then(response => {
-            dispatch(successAction(response.data, SUCCESS_MOVIES_TRENDING));
-        })
-        .catch(error => {
-            dispatch(errorAction(error.message, ERROR_MOVIES_TRENDING));
-        });
-};
-
-export const getMovie = (id, query) => async dispatch => {
-    try {
-        const res = await axios.get(`/api/movies/movie/${id}${query}`);
-
-        dispatch({
-            type: GET_MOVIE_DATA,
-            payload: res.data
-        });
-    } catch (error) {
-        // update in the future :)
-        // dispatch({
-        // 	type: GET_MOVIES_TRENDING,
-        // 	payload: { msg: error.response.statusText, status: error.response.status }
-        // });
-        console.log(error);
-    }
-};
-
-export const searchMovie = (searchQuery, page_number) => async dispatch => {
-    dispatch(requestAction(REQUEST_SEARCH_MOVIES));
-    const fullQuery = `?searchQuery=${searchQuery}&page=${page_number}`;
-    await axios
-        .get(`/api/movies/search${fullQuery}`)
-        .then(response => {
-            dispatch(successAction(response.data, SUCCESS_SEARCH_MOVIES));
-        })
-        .catch(error => {
-            dispatch(errorAction(error.message, ERROR_SEARCH_MOVIES));
-        });
-};
-
-export const searchUsers = searchQuery => async dispatch => {
-    dispatch(requestAction(REQUEST_SEARCH_USERS));
-    await axios
-        .get(`/api/users/search/users/${searchQuery}`)
-        .then(response => {
-            dispatch(successAction(response.data, SUCCESS_SEARCH_USERS));
-        })
-        .catch(error => {
-            dispatch(errorAction(error.message, ERROR_SEARCH_USERS));
-        });
-};
-
-export const clearState = () => async dispatch => {
-    dispatch({
-        type: CLEAR_STATE
+export const getTrendingMovies = (page) => async (dispatch) => {
+  dispatch(requestAction(REQUEST_MOVIES_TRENDING));
+  await axios
+    .get(`/api/movies/trending/movies?page=${page}`)
+    .then((response) => {
+      dispatch(successAction(response.data, SUCCESS_MOVIES_TRENDING));
+    })
+    .catch((error) => {
+      dispatch(errorAction(error.message, ERROR_MOVIES_TRENDING));
     });
+};
+
+export const getPopularMovies = (page) => async (dispatch) => {
+  dispatch(requestAction(REQUEST_MOVIES_TRENDING));
+  await axios
+    .get(`/api/movies/popular/movies?page=${page}`)
+    .then((response) => {
+      dispatch(successAction(response.data, SUCCESS_MOVIES_TRENDING));
+    })
+    .catch((error) => {
+      dispatch(errorAction(error.message, ERROR_MOVIES_TRENDING));
+    });
+};
+
+export const getMovie = (id, query) => async (dispatch) => {
+  try {
+    const res = await axios.get(`/api/movies/movie/${id}${query}`);
+
+    dispatch({
+      type: GET_MOVIE_DATA,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ERROR_MOVIES_TRENDING,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+    console.log(error);
+  }
+};
+
+export const searchMovie = (searchQuery, page_number) => async (dispatch) => {
+  dispatch(requestAction(REQUEST_SEARCH_MOVIES));
+  const fullQuery = `?searchQuery=${searchQuery}&page=${page_number}`;
+  await axios
+    .get(`/api/movies/search${fullQuery}`)
+    .then((response) => {
+      dispatch(successAction(response.data, SUCCESS_SEARCH_MOVIES));
+    })
+    .catch((error) => {
+      dispatch(errorAction(error.message, ERROR_SEARCH_MOVIES));
+    });
+};
+
+export const searchUsers = (searchQuery) => async (dispatch) => {
+  dispatch(requestAction(REQUEST_SEARCH_USERS));
+  await axios
+    .get(`/api/users/search/users/${searchQuery}`)
+    .then((response) => {
+      dispatch(successAction(response.data, SUCCESS_SEARCH_USERS));
+    })
+    .catch((error) => {
+      dispatch(errorAction(error.message, ERROR_SEARCH_USERS));
+    });
+};
+
+export const clearState = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_STATE,
+  });
 };
--- client/src/components/SearchPage/Search/Search.js
+++ client/src/components/SearchPage/Search/Search.js
@@ -1,86 +1,85 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Search.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import Spinner from '../../layout/Spinner/Spinner';
-import { Dropdown } from 'react-bootstrap';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Search.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Spinner from "../../layout/Spinner/Spinner";
+import { Dropdown, DropdownButton } from "react-bootstrap";
 
 const Search = ({
-    search,
-    loading,
-    dropdownState,
-    setDropdownState,
-    ddOptions
+  search,
+  loading,
+  dropdownState,
+  setDropdownState,
+  ddOptions,
 }) => {
-    let timeout = 0;
+  let timeout = 0;
 
-    const handleSearchClick = value => {
-        if (value !== '') {
-            clearTimeout(timeout);
-            search(value, dropdownState);
-        }
-    };
+  const handleSearchClick = (value) => {
+    if (value !== "") {
+      clearTimeout(timeout);
+      search(value, dropdownState);
+    }
+  };
 
-    const handleSearchWait = value => {
-        if (value !== '') {
-            if (timeout) clearTimeout(timeout);
-            timeout = setTimeout(() => {
-                search(value, dropdownState);
-            }, 1300);
-        }
-    };
+  const handleSearchWait = (value) => {
+    if (value !== "") {
+      if (timeout) clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        search(value, dropdownState);
+      }, 1300);
+    }
+  };
 
-    return (
-        <div className='row'>
-            <div className='col-lg-10 col-7'>
-                <div className='form-group has-search'>
-                    <span className='form-control-feedback'>
-                        {loading ? (
-                            <Spinner width='35px' />
-                        ) : (
-                            <FontAwesomeIcon
-                                className='form-conrtrol-feddback'
-                                icon={faSearch}
-                            />
-                        )}
-                    </span>
-                    <input
-                        onKeyPress={e =>
-                            e.key === 'Enter' &&
-                            handleSearchClick(e.target.value)
-                        }
-                        onChange={e => handleSearchWait(e.target.value)}
-                        type='text'
-                        className='form-control'
-                        placeholder='Search'
-                    />
-                </div>
-            </div>
-            <div className='col-lg-2 col-5'>
-                <Dropdown>
-                    <Dropdown.Toggle variant='secondary' id='dropdown-basic'>
-                        Search By {dropdownState}
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        {ddOptions.map((option, index) => (
-                            <Dropdown.Item
-                                key={index}
-                                onClick={() => setDropdownState(option)}
-                            >
-                                {option}
-                            </Dropdown.Item>
-                        ))}
-                    </Dropdown.Menu>
-                </Dropdown>
-            </div>
+  return (
+    <div className="row">
+      <div className="col-lg-9 col-md-7 col-12">
+        <div className="form-group has-search">
+          <span className="form-control-feedback">
+            {loading ? (
+              <Spinner width="35px" />
+            ) : (
+              <FontAwesomeIcon
+                className="form-control-feddback"
+                icon={faSearch}
+              />
+            )}
+          </span>
+          <input
+            onKeyPress={(e) =>
+              e.key === "Enter" && handleSearchClick(e.target.value)
+            }
+            onChange={(e) => handleSearchWait(e.target.value)}
+            type="text"
+            className="form-control"
+            placeholder="Search"
+          />
         </div>
-    );
+      </div>
+      <div className="col-lg-3 col-md-5 col-12">
+        <Dropdown size="sm">
+          <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+            Search By {dropdownState}
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            {ddOptions.map((option, index) => (
+              <Dropdown.Item
+                key={index}
+                onClick={() => setDropdownState(option)}
+              >
+                {option}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    </div>
+  );
 };
 
 Search.propTypes = {
-    search: PropTypes.func.isRequired,
-    loading: PropTypes.bool.isRequired
+  search: PropTypes.func.isRequired,
+  loading: PropTypes.bool.isRequired,
 };
 
 export default Search;
