c1e156aa47898a6f023903414fa94ceb202e3532
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-08-31 19:43:17 +0300

    Update FAQ.js

--- client/src/components/FAQ/FAQ.js
+++ client/src/components/FAQ/FAQ.js
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import "./FAQ.css";
+import './FAQ.css';
 
 class FAQ extends React.Component {
-  render() {
-    return (
-      <div className="container faq-container">
-        <div className="faq-container__title text-center mb-5">
-          <h2>Information about website</h2>
-        </div>
-        <div className="faq-container__main-text mt-3">
-          <div>
-            <h3>What can you do in this website?</h3>
-            <p>
-              You can look thru trending and popular movies. Also you can create
-              new account then rate, write reviews and see other people opinions
-              on films.
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='container faq-container'>
+                <div className='faq-container__title text-center mb-5'>
+                    <h2>Information about website</h2>
+                </div>
+                <div className='faq-container__main-text mt-3'>
+                    <div>
+                        <h3>What can you do in this website?</h3>
+                        <p>
+                            You can look thru trending and popular movies. Also
+                            you can create new account then rate, write reviews
+                            and see other people opinions on films.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default FAQ;
