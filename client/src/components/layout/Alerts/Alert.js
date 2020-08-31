e5f457386474622d280fe964afd928e1400849c6
TautvydasGustas1 <39690430+TautvydasGustas1@users.noreply.github.com>
2020-08-31 21:01:55 +0300

    upd

--- client/src/components/layout/Alerts/Alert.js
+++ client/src/components/layout/Alerts/Alert.js
@@ -2,21 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+const AlertStyle = {
+    boxShadow: '0 2px 4px 1px grey',
+    position: 'fixed',
+    marginTop: '54px',
+};
+
 const Alert = ({ alerts }) =>
-	alerts !== null &&
-	alerts.length > 0 &&
-	alerts.map((alert) => (
-		<div key={alert.id} style={{boxShadow: "0 2px 4px 1px grey"}} className={`conatainer-fluid p-2 text-center bg-${alert.bgColor}  text-light`}>
-			{alert.message}
-		</div>
-	));
+    alerts !== null &&
+    alerts.length > 0 &&
+    alerts.map((alert) => (
+        <div
+            key={alert.id}
+            style={AlertStyle}
+            className={`conatainer-fluid p-2 text-center bg-${alert.bgColor}  text-light`}
+        >
+            {alert.message}
+        </div>
+    ));
 
 Alert.propTypes = {
-	alert: PropTypes.array
+    alert: PropTypes.array,
 };
 
 const mapStateToProps = (state) => ({
-	alerts: state.alert
+    alerts: state.alert,
 });
 
 export default connect(mapStateToProps)(Alert);
